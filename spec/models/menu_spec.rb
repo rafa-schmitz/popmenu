a9e3de117335def601181137bc1eb3ad44858f96
Rafael Schmitz <rafaelsschmitz98@gmail.com>
2025-09-10 00:09:17 -0300

    fix: models spec

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
   describe 'associations' do
-    it { should belong_to(:restaurant).optional }
+    it { should belong_to(:restaurant).required }
     it { should have_many(:menu_menu_items).dependent(:destroy) }
     it { should have_many(:menu_items).through(:menu_menu_items) }
   end
