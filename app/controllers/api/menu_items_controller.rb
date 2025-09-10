d1cbdcabfe15872860db84319b24122cc164f47f
Rafael Schmitz <rafaelsschmitz98@gmail.com>
2025-09-09 23:58:29 -0300

    fix: update API controllers
    - Fix MenuItemsController association name from :restaurants to :restaurant
    - Add proper includes for associations in MenusController

--- app/controllers/api/menu_items_controller.rb
+++ app/controllers/api/menu_items_controller.rb
@@ -1,6 +1,6 @@
 class Api::MenuItemsController < ApplicationController
   def index
-    @menu_items = MenuItem.includes(:menus, :restaurants)
-    render json: @menu_items.as_json(include: [:menus, :restaurants])
+    @menu_items = MenuItem.includes(:menus, :restaurant)
+    render json: @menu_items.as_json(include: [ :menus, :restaurant ])
   end
 end
--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,11 +1,11 @@
 class Api::MenusController < ApplicationController
   def index
     @menus = Menu.includes(:restaurant, :menu_items)
-    render json: @menus.as_json(include: [:restaurant, :menu_items])
+    render json: @menus.as_json(include: [ :restaurant, :menu_items ])
   end
 
   def show
     @menu = Menu.includes(:restaurant, :menu_items).find(params[:id])
-    render json: @menu.as_json(include: [:restaurant, :menu_items])
+    render json: @menu.as_json(include: [ :restaurant, :menu_items ])
   end
 end
